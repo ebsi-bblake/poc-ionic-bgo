0a2f130874deb7f147ed7fc0e7fd5f4c48ece8c1
Boaz Blake <boazblake@gmail.com>
2024-08-21 15:44:11 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'poc-vue-emp',
+  webDir: 'dist'
+};
+
+export default config;
